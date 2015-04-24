713dc1b6a327484d52235c0291b3279fbf3c6b4d
Shino1024 <justincompspec@gmail.com>
2015-04-24 23:05:23 +0200

    Added the bitwise representation of the digits on the 7-segment LED.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 int main(void) {
+	char* digits = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111};
 	PORTA = 0xFF;
 	DDRA = 0xFF;
 	return 0;
